b7f838da9f7fb8a1ec286a6b381282bd475ea854
Cyrilszq <szq1051506383@qq.com>
2017-02-25 00:50:57 +0800

    finish task2 and task3

--- dynamic-data-binding/task2.js
+++ dynamic-data-binding/task2.js
@@ -1,26 +1,35 @@
 class Observer {
-    constructor(options) {
-        this.data = options
-        this.convert(this.data)
+    constructor(data) {
+        this.data = data
+        this.observe(this.data)
+        Object.keys(this.data).forEach(key => {
+            new Watcher(this.data, key)
+        })
     }
 
-    // 遍历对象属性，依次调用defineReactive，为其添加get,set;遍历时如果val仍为对象则递归创建Observer
-    convert(obj) {
-        let keys = Object.keys(obj)
-        for (var i = 0; i < keys.length; i++) {
-            let val = obj[keys[i]]
-            if (Object.prototype.toString.call(val) === '[object Object]') {
-                new Observer(val)
-            }
-            this.defineReactive(keys[i], val)
+    // 遍历对象属性，依次调用defineReactive，为其添加get,set
+    observe(obj) {
+        let self = this
+        // 递归终止条件
+        if (!obj || typeof obj !== 'object') {
+            return;
         }
+        Object.keys(obj).forEach(key => {
+            self.defineReactive(obj, key, obj[key])
+        })
+
     }
 
-    defineReactive(key, val) {
-        Object.defineProperty(this.data, key, {
+    defineReactive(obj, key, val) {
+        let self = this
+        let dep = new Dep()
+        // 如果是一个“比较深”的对象，递归观察
+        self.observe(val)
+        Object.defineProperty(obj, key, {
             enumerable: true,
             configurable: true,
             get() {
+                Dep.target && dep.addSub(Dep.target)
                 console.log('你访问了' + key)
                 return val
             },
@@ -28,7 +37,71 @@ class Observer {
                 console.log(`你设置了${key},新的值为${newVal}`)
                 if (newVal === val) return
                 val = newVal
+                // 继续为新设置的值设置get,set
+                self.observe(newVal)
+                dep.notify(newVal)
             }
         })
     }
+
+    $watch(key, cb) {
+        new Watcher(this.data, key, cb)
+    }
 }
+let $uid = 0
+class Watcher {
+    constructor(obj, key, callback) {
+        this.uid = $uid++
+        Dep.target = this
+
+        if (typeof obj[key] === 'object') {
+            Object.keys(obj[key]).forEach(i => {
+                obj[key][i] //为了触发get，添加依赖
+            })
+        } else {
+            obj[key]
+        }
+        Dep.target = null
+        this.cb = callback
+    }
+
+    update(newVal) {
+        this.cb && this.cb(newVal)
+    }
+}
+
+class Dep {
+    constructor() {
+        this.subs = {}
+    }
+
+    addSub(target) {
+        if (!this.subs[target.uid]) {  //防止重复添加
+            this.subs[target.uid] = target;
+        }
+    }
+
+    notify(newVal) {
+        for (let uid in this.subs) {
+            this.subs[uid].update(newVal);
+        }
+    }
+}
+Dep.target = null
+
+let app2 = new Observer({
+    name: {
+        firstName: 'shaofeng',
+        lastName: 'liang'
+    },
+    age: 25
+});
+
+app2.$watch('name', function (newName) {
+    console.log('我的姓名发生了变化，可能是姓氏变了，也可能是名字变了。')
+});
+
+app2.data.name.firstName = 'hahaha';
+// 输出：我的姓名发生了变化，可能是姓氏变了，也可能是名字变了。
+app2.data.name.lastName = 'blablabla';
+// 输出：我的姓名发生了变化，可能是姓氏变了，也可能是名字变了。
--- dynamic-data-binding/task4.js
+++ dynamic-data-binding/task4.js
@@ -1,10 +1,84 @@
 class Vue {
     constructor(options) {
-        this.el = options.el
+        this.$el = options.el
         this.data = options.data || {}
+        new Compiler({el: this.$el, data: this.data})
     }
 }
 
+// 方便以后扩展，将模板编译单独作为一个类
+class Compiler {
+    constructor(options) {
+        this.$el = options.el && document.querySelector(options.el)
+        this.data = options.data
+        if (this.$el) {
+
+            this.$fragment = this.nodeToFragment(this.$el);
+            this.compile(this.$fragment);
+
+            this.$el.appendChild(this.$fragment);
+        }
+    }
+
+    nodeToFragment(node) {
+        let fragment = document.createDocumentFragment(), child;
+        while (child = node.firstChild) {
+            if (this.isIgnorable(child)) {     // delete '\n'
+                node.removeChild(child);
+            } else {
+                fragment.appendChild(child);   // 移动操作，将child从原位置移动添加到fragment
+            }
+        }
+        return fragment;
+    }
+
+    isIgnorable(node) {
+        var regIgnorable = /^[\t\n\r]+/;
+        return (node.nodeType == 8) || ((node.nodeType == 3) && (regIgnorable.test(node.textContent)));
+    }
+
+    compile(node) {
+        let self = this
+        Array.from(node.childNodes).forEach((child) => {
+            if (child.nodeType === 1) {
+                self.compileElementNode(child)
+            }
+            if (child.nodeType === 3) {
+                self.compileTextNode(child)
+            }
+        })
+    }
+
+    compileTextNode(node) {
+        let text = node.textContent.trim()
+        let exp = parseTextExp(text)
+        // let code = `with (this.data){node.textContent = eval(exp);console.log(node.textContent)}`
+        // new Function(code)
+        // console.log(node.textContent)
+    }
+
+    compileElementNode(node) {
+        this.compile(node)
+    }
+
+}
+
+function parseTextExp(text) {
+    let regText = /\{\{(.+?)\}\}/g;
+    let pieces = text.split(regText);
+    let matches = text.match(regText);
+    // 文本节点转化为常量和变量的组合表达式，PS：表达式中的空格不管，其他空格要保留
+    // 'a {{b+"text"}} c {{d+Math.PI}}' => '"a " + b + "text" + " c" + d + Math.PI'
+    let tokens = [];
+    pieces.forEach(function (piece) {
+        if (matches && matches.indexOf('{{' + piece + '}}') > -1) {    // 注意排除无{{}}的情况
+            tokens.push(piece);
+        } else if (piece) {
+            tokens.push('`' + piece + '`');
+        }
+    });
+    return tokens.join('+');
+}
 
 let app = new Vue({
     el: '#app',
